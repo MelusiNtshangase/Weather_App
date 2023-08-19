a06ca98937c7703b33a39ecb8bbbb75ba8a498ff
MelusiNtshangase <melusintshangase609@gmail.com>
2023-08-19 16:33:09 +0200

    Added dynamic image

--- script.js
+++ script.js
@@ -2,15 +2,19 @@
 
 const apiKey = "b47ecafd449e25914b5164c6b9fe2cad";
 const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const city = document.querySelector(".city");
 const temp = document.querySelector(".temp");
 const humidity = document.querySelector(".humidity");
 const wind = document.querySelector(".wind");
 
-const checkWeather = async function () {
-  const response = await fetch(apiUrl + `&appid=${apiKey}`);
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+const checkWeather = async function (cityName) {
+  const response = await fetch(apiUrl + cityName + `&appid=${apiKey}`);
   const data = await response.json();
 
   city.innerHTML = data.name;
@@ -18,6 +22,27 @@ const checkWeather = async function () {
   humidity.innerHTML = data.main.humidity + "%";
   wind.innerHTML = data.wind.speed + " km/h";
 
+  switch (data.weather[0].main) {
+    case "Clouds":
+      weatherIcon.src = "images/clouds.png";
+      break;
+    case "Clear":
+      weatherIcon.src = "images/clear.png";
+      break;
+    case "Rain":
+      weatherIcon.src = "images/rain.png";
+      break;
+    case "Drizzle":
+      weatherIcon.src = "images/drizzle.png";
+      break;
+    case "Mist":
+      weatherIcon.src = "images/mist.png";
+      break;
+  }
+
   console.log(data);
 };
-checkWeather();
+
+searchBtn.addEventListener("click", function () {
+  checkWeather(searchBox.value);
+});
